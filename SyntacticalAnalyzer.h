451f5281832eae1ae32844f42925b0ead8f96f34
cmeza77306 <31258439+cmeza77306@users.noreply.github.com>
2018-04-03 18:22:47 -0700

    Add files via upload

---
+++ SyntacticalAnalyzer.h
@@ -0,0 +1,50 @@
+#ifndef SYNTACTICALANALYZER
+#define SYNTACTICALANALYZER
+
+#include <stack>
+#include <queue>
+#include <string>
+#include <vector>
+
+using std::stack;
+using std::queue;
+using std::string;
+using std::vector
+
+const int colLen = 36;
+const int rowLen = 45;
+const int cellMax = 15;
+
+
+class SyntacticalAnalyzer
+{
+	private:
+		string cellInput[cellMax];
+		vector<vector<string[cellMax]>> prodTable =
+		{
+			{"Producitons/Tokens"}, {"%%"}, {"function"}, {"["}, {"]"}, {":"}, {"int"}, {"boolean"}, {"real"}, {""}
+
+
+
+
+
+
+
+
+
+		}
+		
+
+
+
+
+
+
+
+};
+
+
+
+
+
+#endif
